b2017c964d172fd3befccf0692b0e27386bc70b5
djzib0 <piotrsabatura@gmail.com>
2021-11-14 14:58:49 +0100

    Add files via upload

--- main_gui.py
+++ main_gui.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from openpyxl import *
+import shutil
 from openpyxl.utils import column_index_from_string
 
 
@@ -18,6 +18,153 @@ def set_xlsx_files_list():
         if "xlsx" in file:
             xlsx_files.append(file)
     return xlsx_files
+
+def check_file_extension_len(file_name):
+    """Checks how long is extension string"""
+    list_ = file_name.split('.')
+    return len(list_[1])
+
+def create_key(list_):
+    """Creates string key with coordinates for copying files."""
+    key = str(list_)
+    return str(key)
+
+def create_short_key(list_):
+    """Creates string key with coordinates from [0:-1]"""
+    key = str(list_[0:-1])
+    return str(key)
+
+def test_print():
+    print("Ni chuja ale to działa.")
+
+def create_folders_and_copy_files(tree, part, name, start):
+    import openpyxl
+    # open excel file
+    book = openpyxl.load_workbook('input.xlsx')
+    sheet = book.active
+    last_row = int(sheet.max_row) # sets last row with data
+
+    drawings = [] # list keeps names of files to be copied
+    EXTENSIONS = ['pdf', 'dwg'] # list of file extensions (TO BE MODIFIED)
+    files_to_copy = []
+    file_list = os.listdir() # creates file list in folder
+
+    checked_row = [0]
+    below_row = [0]
+    current_path = os.getcwd()
+    paths = {'[0]': current_path + '\\new_folder'}
+    paths_short = {'[0]': current_path + '\\new_folder'}
+
+    last_tree_col = tree # value of last column with coorindates
+    part_col = part  # ta wartość będzie domyślna, ale z możliwością modyfikacji, podaje numer kolumny z part number
+    name_col = name # ta wartość będzie domyślna, ale z możliwością modyfikacji, podaje numer kolumny z nazwą części
+    row_counter = start # starting row
+    col_counter = 1 # starting column
+    key = ''
+
+    # adding part/drawing numbers to the list
+
+    # creating list of files valid to be copied
+
+    for file in file_list:
+            files_to_copy.append(file)
+
+    print(files_to_copy)
+
+    # analizing excel file - creating paths for folders and subfolders
+    while row_counter < last_row:
+        checked_row = [0]
+        below_row = [0]
+        for col in range(1, last_tree_col):
+            if sheet.cell(row=row_counter, column=col).value != None: # sprawdza czy wartość komórki jest None
+                checked_row.append(sheet.cell(row=row_counter, column=col).value) # jeżeli nie to dodaje do listy
+            if sheet.cell(row=row_counter+1, column=col).value != None: # sprawdza czy wartość komórki jest None
+                below_row.append(sheet.cell(row=row_counter+1, column=col).value) # jeżeli nie to dodaje do listy
+
+        print(f'test checked {checked_row}')
+        print(f'test_below {below_row}')
+        if len(checked_row) < len(below_row): # sprawdza czy lista podrzędna jest dłuższa
+            #checked_row.append('0') # dodaje do listy pierwszy element, który będzie koordynatą dla bieżącego katalogu
+            print('dodaję koordynaty do słownika i tworzę katalog') # jeżeli tak to dodaje koordynaty do słownika i tworzy katalog
+            # sprawdza czy klucz już występuje w słowniku, jeżeli tak to nie dodaje nowego.
+            print(f'checked row to {checked_row}')
+            key = create_key(checked_row)
+            print('dodaję key')
+            print(f'key to {key}')
+            print(f'to jest checked_row przed short key {checked_row}')
+            short_key = create_short_key(checked_row)
+            print(f'to jest short key  {short_key}')
+            part_number = str(sheet.cell(row=row_counter, column=part_col).value)
+            part_name = str(sheet.cell(row=row_counter, column=name_col).value)
+
+            print(f'checked_row to: {checked_row}')
+
+            print(f'\nTo jest key {key}')
+            print(f'To jest short key {short_key}\n')
+
+            if key not in paths.keys():
+                paths[key] = str(paths[short_key]) + '\\' + part_number + ' ' + part_name
+
+        row_counter += 1
+        #checked_row = []
+        below_row = []
+
+    # creating folders
+    for i, path in paths.items():
+        print(f'to jest klucz {i}')
+        print(f'to jest ścieżka {path}')
+        try:
+            if i != '[0]':
+                os.makedirs(str(path))
+            else:
+                print('to jest katalog startowy')
+        except FileExistsError:
+            print('Ten folder już istnieje')
+
+    # copying files
+    row_counter = 2
+    checked_row = [0]
+
+    print(paths)
+    print(paths_short)
+    while row_counter < last_row:
+        checked_row = [0]
+        below_row = [0]
+        for col in range(1, last_tree_col):
+            if sheet.cell(row=row_counter, column=col).value != None: # sprawdza czy wartość komórki jest None
+                checked_row.append(sheet.cell(row=row_counter, column=col).value) # jeżeli nie to dodaje do listy
+            if sheet.cell(row=row_counter+1, column=col).value != None: # sprawdza czy wartość komórki jest None
+                below_row.append(sheet.cell(row=row_counter+1, column=col).value) # jeżeli nie to dodaje do listy
+        drawing = sheet.cell(row_counter, part_col).value
+        print(f'to jest {drawing}')
+        for file in files_to_copy:
+            if drawing in file:
+                if len(checked_row) < len(below_row):
+                    to_be_copied = str(current_path) + '\\' + str(file)
+                    destination_folder = paths[create_short_key(below_row)]
+                    print(destination_folder)
+                    shutil.copy(to_be_copied, destination_folder)
+                else:
+                    print("tak jest w liście")
+                    to_be_copied = str(current_path) + '\\' + str(file)
+                    destination_folder = paths[create_short_key(checked_row)]
+                    print(f'to jest to be_copied {to_be_copied}')
+                    print(f'to jest kopiowany file {drawing}')
+                    print(str(current_path) + '\\' + str(file))
+                    shutil.copy(to_be_copied, destination_folder)
+
+        row_counter += 1
+
+def create_short_name(text, letters):
+    """Creates shorter name for a given string"""
+    item_list = text.split(" ")
+    new_text = ""
+    print(item_list)
+    for item in item_list:
+        if len(item) <= letters:
+            new_text += item + " "
+        else:
+            new_text += item[0:letters] + " "
 
 
 xlsx_files = set_xlsx_files_list()
@@ -93,7 +240,7 @@ class MainPage(QWidget):
         self.run_script_bttn = QPushButton("Uruchom", self)
         self.run_script_bttn.setFixedWidth(100)
         self.run_script_bttn.setFixedHeight(30)
-        self.run_script_bttn.clicked.connect(self.check_errors)
+        self.run_script_bttn.clicked.connect(self.run_code)
         settings_grid_layout.addWidget(self.run_script_bttn, 8, 0) # add button to grid layout
 
         self.show()
@@ -188,17 +335,38 @@ class MainPage(QWidget):
             msg.exec_()
 
         print(f"To jest licznik błędów: {self.errors_counter}")
+        print(f"To jest self combobox: {self.choose_file_combobox.currentText()}")
 
         return self.errors_counter
+
+
 
 
     def check_col_coord_is_valid(self, coordinate):
         """Checks if column coordinate is valid"""
         return coordinate.isalpha()
 
+    @pyqtSlot()
+    def run_code(self):
+        try:
+            if self.check_errors() == 0:
+                create_folders_and_copy_files(int(self.last_tree_column),
+                                              int(self.part_column),
+                                              int(self.name_column),
+                                              int(self.start_row),
 
-
-
+                                              )
+                print(int(self.last_tree_column))
+                print(int(self.part_column))
+                print(int(self.name_column))
+                print(int(self.start_row))
+        except:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText(f"Oops")
+            msg.setInformativeText('Gibony coś popsuły. Sprawdź dane.')
+            msg.setWindowTitle("Error")
+            msg.exec_()
 
 
 def main():
@@ -206,10 +374,6 @@ def main():
     w = MainPage(title="Aplikacja Piotera")
     sys.exit(app.exec_())
 
-
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
